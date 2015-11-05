533a1829917a4345f6fa5db9c74256ae8ff0291a
Mark <markweig@gmail.com>
2015-11-05 14:24:33 -0500

    Stub Player class and methods
    Added the stubs for the Player Class and update,
    render, and handleInput methods.  Also added allEnemies
    array and player variable.

--- js/app.js
+++ js/app.js
@@ -1,19 +1,17 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+    // Enemy Location
+    // TODO: Randomly generate row, speed, number of enemies?
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    // Enemy Image
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // multiply any movement by the dt parameter
+    // which will ensure the game runs at same speed
 };
 
 // Draw the enemy on the screen, required method for game
@@ -21,15 +19,30 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
+// Player class
+var Player = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+};
+
+Player.prototype.update = function() {
+    // TODO: Write the update function
+};
+
+Player.prototype.render = function() {
+    // TODO: Write the render function
+};
+
+Player.prototype.handleInput = function() {
+    // TODO: Write the function
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [];
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
